6dba9cd743148eeb6a179f85b0567729cad3bf2b
AKSHAY BISHT <akshay.bisht.dev@gmail.com>
2024-03-21 15:16:19 +0530

    fix some bugs in header file (responsive code)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import React, { useContext, useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import { Cart } from '../context/Context'
-import { RiMenu2Line } from "react-icons/ri";
+import React, { useContext, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Cart } from '../context/Context';
+import { RiMenu2Line } from 'react-icons/ri';
 
 const Header = () => {
     const { dish } = useContext(Cart);
@@ -9,26 +9,47 @@ const Header = () => {
 
     const toggleMenu = () => {
         setToggle(!toggle);
-    }
+    };
 
     return (
         <nav className='px-4 max-w-full flex justify-between items-center h-[50px] mx-auto'>
-            <NavLink to='/' className='text-2xl'>Food <span className='text-red-600 font-bold'>X</span></NavLink>
+            <NavLink to='/' className='text-2xl'>
+                Food <span className='text-red-600 font-bold'>X</span>
+            </NavLink>
 
-            <div className={toggle ? 'h-screen fixed left-0 right-0 top-0 bottom-0 z-10' : 'sm:block hidden'}>
-                <ul className={toggle ? 'flex flex-col gap-4 h-screen justify-center items-center bg-black text-white' : 'flex gap-4'}>
-                    <NavLink to='/' onClick={() => setToggle(!toggle)}>Home</NavLink>
-                    <NavLink to='/products' onClick={() => setToggle(!toggle)}>Products</NavLink>
-                    <NavLink to='/category' onClick={() => setToggle(!toggle)}>Category</NavLink>
-                    <NavLink to='/cart' onClick={() => setToggle(!toggle)}>Cart <span>({dish.length})</span></NavLink>
+            <div className='sm:flex hidden'>
+                <ul className='flex gap-4'>
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/products'>Products</NavLink>
+                    <NavLink to='/category'>Category</NavLink>
+                    <NavLink to='/cart'>Cart <span>({dish.length})</span></NavLink>
                 </ul>
             </div>
 
-            <div className='sm:hidden block z-10 text-red-600 font-bold text-2xl' onClick={toggleMenu}>
+            <div className='sm:hidden block z-50 text-red-600 font-bold text-2xl' onClick={toggleMenu}>
                 <RiMenu2Line />
             </div>
-        </nav>
-    )
-}
 
-export default Header
+            {toggle && (
+                <div className='sm:hidden fixed inset-0 z-10 bg-black bg-opacity-95' onClick={toggleMenu}>
+                    <div className='flex flex-col gap-4 h-full justify-center items-center text-white'>
+                        <NavLink to='/' onClick={toggleMenu}>
+                            Home
+                        </NavLink>
+                        <NavLink to='/products' onClick={toggleMenu}>
+                            Products
+                        </NavLink>
+                        <NavLink to='/category' onClick={toggleMenu}>
+                            Category
+                        </NavLink>
+                        <NavLink to='/cart' onClick={toggleMenu}>
+                            Cart <span>({dish.length})</span>
+                        </NavLink>
+                    </div>
+                </div>
+            )}
+        </nav>
+    );
+};
+
+export default Header;
